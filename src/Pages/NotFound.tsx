ba9794887a9ae433dc6bf86384a099b2309a4eb6
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2021-07-15 02:28:13 +0530

    further PAge  Created

---
+++ src/Pages/NotFound.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { memo } from 'react';
+
+interface Props{
+
+}
+
+const NotFound:React.FC<Props>=(props) => {
+    return(
+        <>
+        helloeesdfrs
+        </>
+    );
+
+}
+
+NotFound.defaultProps={
+    
+}
+export default memo(NotFound);
