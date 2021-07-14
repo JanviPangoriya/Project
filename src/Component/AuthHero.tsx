a1f17083fae3e5e7d10b0d1198974de1f4097bc8
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2021-07-15 02:18:16 +0530

    AuthHero Added

---
+++ src/Component/AuthHero.tsx
@@ -0,0 +1,21 @@
+import React, { memo } from 'react';
+
+interface Props{
+
+}
+
+const AuthHero:React.FC<Props>=(props) => {
+    return(
+        <div className="bg-gray-900 h-screen w-1/2">
+        <div className="mt-60 mx-40 transform -rotate-45 text-8xl font-bold  bg-red-500 text-transparent outline-white bg-clip-text">
+            Hero<br/>Section
+        </div>
+                </div>
+    );
+
+}
+
+AuthHero.defaultProps={
+    
+}
+export default memo(AuthHero);
