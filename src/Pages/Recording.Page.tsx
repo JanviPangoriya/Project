e514a1d7f78596ab6a50be771e6f4f136c53d14a
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2021-07-15 02:27:25 +0530

    App Container PAge  Created

---
+++ src/Pages/Recording.Page.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { memo } from 'react';
+import { Link } from 'react-router-dom';
+
+interface Props{
+
+}
+
+const Recording:React.FC<Props>=(props) => {
+    return(
+        <div className="pt-10">
+        <div className="text-gray-800 pt-80 pl-20 font-bold">
+            <div className="absolute top-40 text-5xl pl-10 uppercase text-transparent bg-gradient-to-br from-red-700 to-blue-500 bg-clip-text font-extrabold">
+                Welcome to Recording Page
+                </div>
+                </div>
+                <div className="absolute ml-40 space-y-5">
+            Want to go to dashboard ?  
+                
+                <Link to="/dashboard">
+                    
+                    <button className="pl-4 text-green-500 font-bold hover:text-red-600">
+                        DashBoard
+                    </button>
+                </Link>
+        </div>
+        </div>
+    );
+
+}
+
+Recording.defaultProps={
+    
+}
+export default memo(Recording);
