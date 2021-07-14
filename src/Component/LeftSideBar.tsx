85b0d66d61dbec2e717791b4b9c09a06b57f5990
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2021-07-15 02:24:25 +0530

    Leftsidebar Created

---
+++ src/Component/LeftSideBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { memo } from 'react';
+
+interface Props{
+
+}
+
+const LeftSideBar:React.FC<Props>=(props) => {
+    return(
+        <div className="h-screen pt-10 w-1/5 bg-gray-500">
+            
+        </div>
+    );
+
+}
+
+LeftSideBar.defaultProps={
+    
+}
+export default memo(LeftSideBar);
