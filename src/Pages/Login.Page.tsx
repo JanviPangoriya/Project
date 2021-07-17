4ca9e6cecd9b18f8485f3cda94eb753d5a0d8871
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2021-07-17 13:58:34 +0530

    updated

--- src/Pages/Login.Page.tsx
+++ src/Pages/Login.Page.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
 import { memo } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { HiLockClosed, HiUser } from "react-icons/hi";
+import { FaSpinner} from "react-icons/fa";
+import { useState } from 'react';
 interface Props{
 
 }
 const Login:React.FC<Props>=(props) => {
+  const [data,setData] = useState({email:"",password:""})
+  
+const [touched,setTouched] = useState({email:false,password:false})
+
+const[submitting,setSubmitting] = useState(false);
+
+let history = useHistory();
+const handlechange = (e : React.ChangeEvent<HTMLInputElement>) =>{
+  const nameOfChangeInput = e.target.name;
+setData({...data,[nameOfChangeInput]:e.target.value})
+}
+
+
+const handleBlur = (e : React.FocusEvent<HTMLInputElement>) =>{
+  const nameOfBlurInput = e.target.name;
+setTouched({...touched,[nameOfBlurInput]:true})}
+const allowed=()=>{
+  if (emailError === '' && passwordError ==='' ){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+let emailError = "";
+let passwordError = "";
+
+if(!data.email)
+{
+emailError = "Email is required"
+}
+else if(!data.email.endsWith("@gmail.com"))
+{
+emailError = "Please enter a valid email address"
+}
+
+if(!data.password)
+{
+  passwordError = "Password is required"
+}
+else if(data.password.length < 8)
+{
+  passwordError = "Password should be atleast 8 characters"
+}
     return(
         <>
         <div className="lg:w-1/2">
@@ -16,21 +62,46 @@ const Login:React.FC<Props>=(props) => {
                           <Link to="/sign" className="border-b-2 ml-2 border-blue-500 text-blue-500">
                               Create an account
                           </Link> </div>
-                     <form action="#">
+                     <form className="" onSubmit={(e) =>
+    {
+      e.preventDefault();
+      if(emailError || passwordError)
+      {
+        console.log("rejceted")
+        return;
+      }
+     setSubmitting(true);
+        setTimeout( ()=>{
+      console.log(data);
+
+      history.push("/dashboard");
+      },5000);
+    }}>
                      <div className="h-20 mb-1 pt-3 pb-6">
           <label htmlFor="email-address" className="sr-only">Email address</label>
-          <div className="flex flex-row ">
           <HiUser className="mb-10 z-50 m-2 w-7 h-7 text-blue-500 absolute"></HiUser>
           <input id="email-address" name="email" type="email" autoComplete="email" required className="relative appearance-none rounded-none block p-10 w-full h-12 pl-10 py-5 border-b border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500  focus:border-indigo-500 focus:z-10 sm:text-sm" 
-          value ={}
+          value ={data.email}
+          onChange = {handlechange}
+          onBlur = {handleBlur}
           placeholder="Email address" />
-          </div>
+        {touched.email && <div className="text-red-600 font-semibold ml-10 mt-1">
+            {emailError}
+          </div>}
         </div>
         <div  className="h-20 mb-2 pt-3 pb-6">
           <label htmlFor="password" className="sr-only">Password</label>
           <HiLockClosed className="mb-10 z-50 m-2 w-7 h-7 text-blue-500 absolute"></HiLockClosed>
-          <input id="password" name="password" type="password" autoComplete="current-password" required className=" relativeappearance-none rounded-none relative block p-10 w-full h-12 pl-10 py-2 border-b border-gray-300 placeholder-gray-700 placeholder-opacity-95  text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+          <input id="password" name="password" type="password" autoComplete="current-password" required className=" relativeappearance-none rounded-none relative block p-10 w-full h-12 pl-10 py-2 border-b border-gray-300 placeholder-gray-700 placeholder-opacity-95  text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+           value ={data.password}
+           onChange = {handlechange}
+           onBlur = {handleBlur}
+          placeholder="Password" />
+          { touched.password && <div className="text-red-600 font-semibold ml-10 mt-1">
+            {passwordError}
+          </div>}
         </div>
+        
         <div className="flex flex-col justify-start md:flex-row md:justify-between md:items-center mt-5 "
   > <div className="flex-row flex ">
     <span className="mr-5">Show Password</span>
@@ -40,8 +111,8 @@ const Login:React.FC<Props>=(props) => {
       
     </div>
   </div>
-    <button
-type="submit" className="w-20 px-3 mt-5 md:mt-0 py-1 rounded-md bg-blue-600 text-white font-semibold shadow-2xl hover:shadow-none">
+    <button  disabled={!allowed()}
+    type="submit" className={"w-20 px-3 mt-5 md:mt-0 py-1 rounded-md bg-blue-600 text-white font-semibold shadow-2xl hover:shadow-none "} >
 Log In
 </button>
   </div>
@@ -55,6 +126,7 @@ Log In
   <div className="text-center mt-4 text-blue-500 font-bold text-md">
       Forgot Password?
   </div>
+  {submitting && <FaSpinner className ="animate-spin mx-auto mt-2"></FaSpinner>}
   <div className="mt-20">
 © 2020 All Rights Reserved. <span className="text-blue-500 font-bold text-md">Devslane</span> is a comapny which is currently working on web development. <span className="text-blue-500 font-bold text-md">Cookie Preferences <span className="text-gray-700 font-normal">,</span> Privacy<span className="text-gray-700 font-normal">,</span> and Terms.</span>
   </div>
