e9f3364f550f1453e6a0cb4084669c1fbac53f1d
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2021-07-22 02:00:44 +0530

    changes done

--- src/Component/AvatarList/AvatarList.tsx
+++ src/Component/AvatarList/AvatarList.tsx
@@ -1,45 +1,54 @@
 import React from "react";
-interface props {
-  l?: Array<string>;
-}
-const Avatararray: React.FC<props> = ({ l }) => {
-    var s=0;
-  if (l && l.length > 0) {
-    s = l.length - 4;
-  }
 
+const image =
+  "https://designreset.com/cork/ltr/demo4/assets/img/profile-12.jpeg";
+interface Props {
+  avatarlength?: number;
+  className?: string;
+}
+const AvatarList: React.FC<Props> = ({ avatarlength, className }) => {
+  function fillArray(value: string, len: number) {
+    if (len == 0) return [];
+    var a = [value];
+    while (a.length * 2 <= len) a = a.concat(a);
+    if (a.length < len) a = a.concat(a.slice(0, len - a.length));
+    return a;
+  }
+  var avatarArray = fillArray(image, avatarlength!);
+  var remaining = 0;
+  if (avatarArray && avatarArray.length > 0) {
+    remaining = avatarArray.length - 4;
+  }
   return (
     <>
-      <div className="flex flex-row mt-20 items-center">
-        {l &&
-          l.slice(0, 4).map((k, index: number) => (
+      <div className={"flex flex-row items-center mt-28 " + className}>
+        {avatarArray &&
+          avatarArray.slice(0, 4).map((avatar, index) => (
             <div>
-              {console.log(index)}
               <img
+                src={avatar}
                 key={index}
-                src={k}
+                alt=""
                 className={
                   "relative" +
                   " -left-" +
                   `${index * 8}` +
-                  " rounded-full transform  hover:-translate-y-5  duration-1000"
+                  " transform duration-1000 hover:-translate-y-4 rounded-full "
                 }
               ></img>
             </div>
           ))}
-              {s > 0 &&
-                  <div className="w-28 h-12 relative -left-32  bg-white shadow-2xl font-medium text-blue-500 rounded-full flex items-center justify-center">
-                      {" "}
-                      +{l && l.length - 4} more
-                  </div>}
+        {remaining > 0 && (
+          <div className="w-28 h-12 relative -left-28 border-2 border-gray-500  bg-white shadow-2xl font-medium text-blue-500 rounded-full flex items-center justify-center">
+            + {remaining} more
+          </div>
+        )}
       </div>
     </>
   );
 };
-const v = "https://designreset.com/cork/ltr/demo4/assets/img/profile-12.jpeg";
 
-Avatararray.defaultProps = {
-  l: [v, v, v, v, v, v, v],
+AvatarList.defaultProps = {
+  avatarlength: 5,
 };
-
-export default React.memo(Avatararray);
+export default AvatarList;
