a65093d6b8590c4fc9da82ee3971db0741091edf
JanviPangoriya <janvi.pangoriya_cs18@gla.ac.in>
2021-08-07 01:02:22 +0530

    changes done

--- src/store/actions/groups.actions.ts
+++ src/store/actions/groups.actions.ts
@@ -3,14 +3,14 @@ import { store } from "..";
 import { GROUP_QUERY, GROUP_QUERY_COMPLETED } from "../../constant";
 import { Group } from "../../model/Group";
 
-const groupsQueryAction = (query:string) => ({ type: GROUP_QUERY, payload: query });
-const groupsQueryCompletedAction = (query: string, groups: Group[]) => ({ type: GROUP_QUERY_COMPLETED, payload: { query, groups } });
+const queryAction = (query:string) => ({ type: GROUP_QUERY, payload: query });
+const queryCompletedAction = (query: string, groups: Group[]) => ({ type: GROUP_QUERY_COMPLETED, payload: { query, groups } });
 
 
 export const groupAction = bindActionCreators(
   {
-    query: groupsQueryAction,
-    querCompleted: groupsQueryCompletedAction,
+    query: queryAction,
+    querCompleted: queryCompletedAction,
   },
   store.dispatch
 );
